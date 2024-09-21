4709fa2390a865c6a0c1970da1e3ec496661f693
Sebastian Andrade <sandradecedano@gmail.com>
2024-09-21 18:36:00 -0500

    Added cors

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,20 @@
 import express from "express";
+import cors from 'cors';
 
 import loadData from "./libs/LoadData.js";
+import { FRONTEND_URL } from "./config.js";
 
 const app = express();
+const BooksData = loadData();
+
+app.use(cors({
+    credentials: true,
+    origin: FRONTEND_URL,
+}));
+app.use(express.json());
 
 app.get("/books", (_, res) => {
-    const data = loadData();
-    res.json(data);
+    res.json(BooksData);
 });
 
 app.get("/books/:index", (req, res) => {
